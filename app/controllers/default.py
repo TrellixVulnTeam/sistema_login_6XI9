d4f35156e31cd7fb3e090f675aa825eecab0e946
dvessoni <dvessoni@gmail.com>
2018-02-17 17:02:12 -0200

    adicionado icone e consertado a pagina 404

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -3,6 +3,8 @@ from app.models.tables import User
 from bottle import request, template
 from bottle import static_file
 from bottle import error
+from bottle import redirect
+
 
 #html = '<center><h1> Olá </h1></center>'
 '''
@@ -37,25 +39,42 @@ def fonts(filename):
 
 @app.route('/') # @get('/')
 def login():
-    return template('login')
+    return template('login', sucesso=True)
 
 @app.route('/cadastro')
 def cadastro():
-    return template('cadastro')
+    return template('cadastro', existe_username=False)
 
 @app.route('/cadastro', method='POST')
 def acao_cadastro(db):
     username = request.forms.get('username')
     password = request.forms.get('password')
-    new_user = User(username,password)
-    db.add(new_user)
-    return template('verificacao_cadastro', nome=username)
+    try:
+        db.query(User).filter(User.username == username).one()
+        existe_username = True
+    except:
+        existe_username = False
+    
+    if not existe_username:
+        new_user = User(username,password)
+        db.add(new_user)
+        return redirect('/usuarios')
+    return template('cadastro', existe_username=True)
 
 @app.route('/', method='POST') # @post('/')
-def acao_login():
+def acao_login(db):
     username = request.forms.get('username')
     password = request.forms.get('password')
-    return template('verificacao_login', sucesso=True)
+    result = db.query(User).filter((User.username == username) & (User.password == password)).all()
+    
+    if result:
+        return redirect('/usuarios')
+    return template('login',sucesso=False)
+
+@app.route('/usuarios')
+def usuarios(db):
+    usuarios = db.query(User).all()
+    return template('lista_usuarios', usuarios=usuarios)
 
 @app.error(404)
 def error404(error):
